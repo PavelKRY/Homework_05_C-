0537e31d7bd77d1c0a140cce7fc96300bc212168
PavelKRY <kryukov.p14@gmail.com>
2022-09-06 18:00:36 +0500

    issue task_03 in new branch

---
+++ Task_03_reshenie+)/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте массив натуральных чисел.
+//Найдите разницу между максимальным и минимальным элементов массива.
+//[3 7 22 2 78] -> 76
+
+int N = 100;
+int[] array = new int [N];
+array[0] = 1;
+Console.WriteLine(array[0]);
+for(int i = 1; i < N; i++)
+{
+    array[i] = array[i-1]+1;
+    Console.WriteLine(array[i]);
+}
+int answer = array[N-1] - array[0];
+Console.WriteLine(answer);
