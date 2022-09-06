fd1f77aaf2ed85791ca7d022d6a0aeb4358779e7
PavelKRY <kryukov.p14@gmail.com>
2022-09-06 20:00:32 +0500

    Additional task 3 issue

---
+++ Additional_task_Easy_03/Program.cs
@@ -0,0 +1,21 @@
+﻿// Сгенерировать массив из N целых чисел от -100 до 100. Выясните,
+// содержатся ли в массиве отрицательные числа. На экран вывести только ответ:
+// «Да» или «Нет».
+
+int N = 201;
+int[] array = new int [N];
+array[0] = -100;
+Console.WriteLine(array[0]);
+for(int i = 1; i < N; i++)
+{
+    array[i] = array[i-1]+1;
+    Console.WriteLine(array[i]);
+}
+for(int i = 0; i < N; i++)
+{
+    if (array[i] < 0)
+    {
+    Console.WriteLine("yes");
+    break;
+    }
+}
